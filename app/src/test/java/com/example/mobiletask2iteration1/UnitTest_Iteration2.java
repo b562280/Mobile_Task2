fdde13bbbd0f506cd2ef6f5e5f58cc4f6d62339b
DIGITUSER <b562280@my.leedscitycollege.ac.uk>
2022-05-04 15:37:23 +0100

    Update testing

--- app/src/test/java/com/example/mobiletask2iteration1/UnitTest_Iteration2.java
+++ app/src/test/java/com/example/mobiletask2iteration1/UnitTest_Iteration2.java
@@ -17,33 +17,69 @@ import java.util.Random;
 public class UnitTest_Iteration2 {
 
     @Test
-    public void MeterReadingJob_Test_SettersAndGetters() {
+    public void Test_MeterReadingJob_DeadlineDate() {
         MeterReadingJob JOB = addJob();
-
-        JOB.setMeterType(2);
-        assertEquals("Solar",JOB.getMeterTypeString());
 
         JOB.setDeadlineDate("20/02/2019");
         assertEquals("20/02/2019",JOB.getDeadlineDateString());
+    }
+
+    @Test
+    public void Test_MeterReadingJob_Address() {
+        MeterReadingJob JOB = addJob();
 
         JOB.setAddress("Address");
         assertEquals("Address",JOB.getJobAddress());
+    }
+
+    @Test
+    public void Test_MeterReadingJob_MeterLocation() {
+        MeterReadingJob JOB = addJob();
 
         JOB.setMeterLocation("Basement");
         assertEquals("Basement",JOB.getMeterLocation());
+    }
+
+    @Test
+    public void Test_MeterReadingJob_UtilityCompany() {
+        MeterReadingJob JOB = addJob();
 
         JOB.setUtilComp("Utility Company");
         assertEquals("Utility Company",JOB.getUtilityCompany());
+    }
+
+    @Test
+    public void Test_MeterReadingJob_JobStatus() {
+        MeterReadingJob JOB = addJob();
 
         JOB.setJobStatus(true);
         assertEquals(true,JOB.getJobStatus());
+    }
+
+    @Test
+    public void Test_MeterReadingJob_CustomerName() {
+        MeterReadingJob JOB = addJob();
 
         JOB.setCustomerName("Jake Morris");
         assertEquals("Jake Morris",JOB.getNameOfCustomer());
     }
 
     @Test
-    public void ReplaceJobInMeterReadingList() {
+    public void Test_MeterReadingJob_MeterType() {
+        MeterReadingJob JOB = addJob();
+
+        JOB.setMeterType(0);
+        assertEquals("Gas",JOB.getMeterTypeString());
+
+        JOB.setMeterType(1);
+        assertEquals("Electric",JOB.getMeterTypeString());
+
+        JOB.setMeterType(2);
+        assertEquals("Solar",JOB.getMeterTypeString());
+    }
+
+    @Test
+    public void Test_MeterReadingJob_List_ReplaceJob() {
         MeterReadingJob JOB_1 = addJob();
         MeterReadingJob JOB_2 = addJob();
 
